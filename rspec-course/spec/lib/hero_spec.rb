e83e4cb48308ff0e5933ea9fa328bbfb3195852e
Oseas Oliveira <ozzyoliv@gmail.com>
2024-07-19 16:45:10 -0300

    feat(course): Adding RSpec test 'has more than 1000 HP points'

--- rspec-course/spec/lib/hero_spec.rb
+++ rspec-course/spec/lib/hero_spec.rb
@@ -8,4 +8,9 @@ describe Hero do
     hero = Hero.new
     expect(hero.weapon).to eq('sword')
   end
+
+  it 'has more than 1000 HP points' do
+    hero = Hero.new
+    expect(hero.life).to be > 1000
+  end
 end
