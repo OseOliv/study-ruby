3c09bd00a687c34f0d5907d3ed7593ded7751f97
Oseas Oliveira <ozzyoliv@gmail.com>
2024-07-20 15:50:18 -0300

    feat(exercise): Adding Two Sum Exercise Solution

---
+++ 20_algorithms/lib/two_sum.rb
@@ -0,0 +1,17 @@
+
+class TwoSum
+  def two_sum(nums, target)
+    hash_num = Hash.new(0)
+
+    nums.each_with_index do |num, index|
+      compl = target - num
+      if hash_num.has_key?(compl)
+        return [hash_num[compl], index]
+      else
+        hash_num[num] = index
+      end
+    end
+  end
+end
+
+
