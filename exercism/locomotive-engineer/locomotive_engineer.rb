0b3bd1ea6f6afa1a338d6aefd96764a24724e09c
Oseas Oliveira <ozzyoliv@gmail.com>
2024-07-26 16:54:21 -0300

    feat(exercise): Adding Exercism Locomotive Engineer Solution

--- exercism/locomotive-engineer/locomotive_engineer.rb
+++ exercism/locomotive-engineer/locomotive_engineer.rb
@@ -1,17 +1,18 @@
 class LocomotiveEngineer
-  def self.generate_list_of_wagons
-    raise 'Please implement the LocomotiveEngineer.generate_list_of_wagons method'
+  def self.generate_list_of_wagons(*wagons)
+    wagons
   end
 
   def self.fix_list_of_wagons(each_wagons_id, missing_wagons)
-    raise 'Please implement the LocomotiveEngineer.fix_list_of_wagons method'
+    first_wagon, second_wagon, third_wagon, *rest_of_wagon = each_wagons_id
+    [third_wagon, *missing_wagons, *rest_of_wagon, first_wagon, second_wagon]
   end
 
-  def self.add_missing_stops
-    raise 'Please implement the LocomotiveEngineer.add_missing_stops method'
+  def self.add_missing_stops(transport_route, **stops)
+    {**transport_route, stops: stops.values}
   end
 
   def self.extend_route_information(route, more_route_information)
-    raise 'Please implement the LocomotiveEngineer.extend_route_information method'
+    {**route, **more_route_information}
   end
 end
