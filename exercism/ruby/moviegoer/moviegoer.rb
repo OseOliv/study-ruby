a002a1eb03f4d3c452ae6d5051239113dafed424
Oseas Oliveira <ozzyoliv@gmail.com>
2024-07-26 17:49:48 -0300

    feat(exercise): Adding Exercism Moviegoer Solution

--- exercism/ruby/moviegoer/moviegoer.rb
+++ exercism/ruby/moviegoer/moviegoer.rb
@@ -9,15 +9,15 @@ class Moviegoer
   end
 
   def ticket_price
-    raise 'Please implement the Moviegoer#ticket_price method'
+    @age >= 60 ? 10 : 15
   end
 
   def watch_scary_movie?
-    raise 'Please implement the Moviegoer#watch_scary_movie method'
+    @age < 18 ? false : true
   end
 
   # Popcorn is 🍿
   def claim_free_popcorn!
-    raise 'Please implement the Moviegoer#claim_free_popcorn method'
+    @member ? "🍿" : (raise NotMovieClubMemberError)
   end
 end
