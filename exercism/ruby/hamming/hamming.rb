dbe11668161dadf3cd3d375ba88df2e6253a6560
Oseas Oliveira <ozzyoliv@gmail.com>
2024-07-28 12:16:50 -0300

    feat(exercism): Adding Exercism Hamming Soltution

--- exercism/ruby/hamming/hamming.rb
+++ exercism/ruby/hamming/hamming.rb
@@ -1,7 +1,21 @@
-=begin
-Write your code for the 'Hamming' exercise in this file. Make the tests in
-`hamming_test.rb` pass.
+class Hamming
+  def self.compute(dna1, dna2)
+    if dna1.length != dna2.length
+      raise ArgumentError
+    end
 
-To get started with TDD, see the `README.md` file in your
-`ruby/hamming` directory.
-=end
+    count_differences = 0
+
+    dna1.chars.each_with_index do |char1, index|
+      if char1 != dna2[index]
+        count_differences += 1
+      end
+
+    end
+
+    count_differences
+
+  end
+
+end
+
