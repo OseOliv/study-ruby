4eaf9159c5ac8c4a20d88eb081465b5ea4c881e9
Oseas Oliveira <ozzyoliv@gmail.com>
2024-07-28 11:48:09 -0300

    feat(exercism): Adding Exercism Matrix Solution

--- exercism/ruby/matrix/matrix.rb
+++ exercism/ruby/matrix/matrix.rb
@@ -1,7 +1,13 @@
-=begin
-Write your code for the 'Matrix' exercise in this file. Make the tests in
-`matrix_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/matrix` directory.
-=end
+class Matrix
+  attr_reader :rows, :columns
+  def initialize(text)
+    @rows = text.lines.map { |l| l.split.map(&:to_i) }
+    @columns = rows.transpose
+  end
+  def row(offset)
+    rows[offset-1]
+  end
+  def column(offset)
+    columns[offset-1]
+  end
+end
