556048b619e0759adc68985f7a117dca72f4cd08
Oseas Oliveira <ozzyoliv@gmail.com>
2024-07-31 13:46:17 -0300

    feat: adding fibonacci search exercise solution

--- 20_algorithms/lib/fibonacci_search.rb
+++ 20_algorithms/lib/fibonacci_search.rb
@@ -1,53 +1,46 @@
-def fibonacci_search(array, x)
-  n = array.length
+class FibonacciSearch
+  def fibonacci_search(array, x)
+    n = array.length
 
-  # Inicialize os números de Fibonacci
-  fibM2 = 0
-  fibM1 = 1
-  fibM = fibM2 + fibM1
-
-  # Encontre o menor número de Fibonacci maior ou igual ao tamanho do array
-  while fibM < n
-    fibM2 = fibM1
-    fibM1 = fibM
+    fibM2 = 0
+    fibM1 = 1
     fibM = fibM2 + fibM1
-  end
 
-  # Marca o início da sublista eliminada
-  offset = -1
-
-  # Enquanto houver elementos para inspecionar
-  while fibM > 1
-    # Índice a comparar
-    i = [offset + fibM2, n - 1].min
-
-    if array[i] < x
-      fibM = fibM1
-      fibM1 = fibM2
-      fibM2 = fibM - fibM1
-      offset = i
-    elsif array[i] > x
-      fibM = fibM2
-      fibM1 = fibM1 - fibM2
-      fibM2 = fibM - fibM1
-    else
-      return i
+    while fibM < n
+      fibM2 = fibM1
+      fibM1 = fibM
+      fibM = fibM2 + fibM1
     end
-  end
 
-  # Comparação final
-  if fibM1 && array[offset + 1] == x
-    return offset + 1
-  end
+    offset = -1
 
-  # Elemento não encontrado
-  return -1
+    while fibM > 1
+      i = [offset + fibM2, n - 1].min
+
+      if array[i] < x
+        fibM = fibM1
+        fibM1 = fibM2
+        fibM2 = fibM - fibM1
+        offset = i
+      elsif array[i] > x
+        fibM = fibM2
+        fibM1 = fibM1 - fibM2
+        fibM2 = fibM - fibM1
+      else
+        return i
+      end
+    end
+
+
+    if fibM1 && array[offset + 1] == x
+      return offset + 1
+    end
+
+    return -1
+  end
 end
 
 
-array = [2, 3, 4, 10, 40]
-x = 10
 
-puts fibonacci_search(array, x) # Output esperado: 3
 
 
