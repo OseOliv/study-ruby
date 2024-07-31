b46ff53b440dc3110ca76068500289af61c9b8a6
Oseas Oliveira <ozzyoliv@gmail.com>
2024-07-31 16:01:37 -0300

    feat: adding method  to see the monthly savings

--- apps/lib/orcamento/analysis.rb
+++ apps/lib/orcamento/analysis.rb
@@ -12,4 +12,13 @@ class Analysis
     month.values_at(:rent_bill, :water_bill, :energy_bill, :internet_bill, :food).sum
   end
 
+  def monthly_savings
+    @expenses_history.map do |month|
+      total_expenses = total_expenses_for_month(month)
+      savings = monthly_budget - total_expenses
+      {month: month, savings: savings}
+    end
+  end
+
+
 end
