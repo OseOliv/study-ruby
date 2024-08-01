5a27d00b266c5c51e0287058aa5f5cc8fa13598e
Oseas Oliveira <ozzyoliv@gmail.com>
2024-08-01 11:27:24 -0300

    feat: add method to distribute tasks to participants

--- apps/lib/planejador/organizer.rb
+++ apps/lib/planejador/organizer.rb
@@ -9,13 +9,24 @@ end
 class TaskOrganizer
   def initialize
     @participants = []
-    @task = []
+    @tasks = []
   end
   def add_participant(participant)
     @participants << participant
   end
   def add_task(task)
-    @task << task
+    @tasks << task
+  end
+
+  def distribute_tasks
+    return {} if @participants.empty? || @tasks.empty?
+
+    tasks_distribution = Hash.new { |hash, key| hash[key] = [] }
+    @tasks.each_with_index do |task, index|
+      participant = @participants[index % @participants.length]
+      tasks_distribution[participant.name] << task.description
+    end
+    tasks_distribution
   end
 
 end
